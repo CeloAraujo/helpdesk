fa61e1a93e93d960bccfc44a0975bc9b13ccdd27
CeloAraujo <celoparaujo99@gmail.com>
2024-08-05 19:49:15 -0300

    componente tecnico-create criado

---
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-create',
+  template: `
+    <p>
+      tecnico-create works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TecnicoCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
