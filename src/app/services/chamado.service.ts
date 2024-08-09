415600729af95558ebc23353b9bb0a7c6076f941
CeloAraujo <celoparaujo99@gmail.com>
2024-08-09 20:12:45 -0300

    criando novo chamado finalizado

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -8,11 +8,23 @@ import { API_CONFIG } from '../config/api.config';
   providedIn: 'root'
 })
 export class ChamadoService {
-
-
+  
+  
   constructor(private http: HttpClient) { }
-
+  
   findAll():Observable<Chamado[]>{
     return this.http.get<Chamado[]>(`${API_CONFIG.baseURL}/chamados`);
+  }
+
+ create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${API_CONFIG.baseURL}/chamados`, chamado);
+  }
+  
+  update(id: string, chamado: Chamado): Observable<void> {
+    return this.http.put<void>(`${API_CONFIG.baseURL}/chamados/${id}`, chamado);
+  }
+  
+  delete(id: any): Observable<Chamado> {
+    return this.http.delete<Chamado>(`${API_CONFIG.baseURL}/chamados/${id}`);
   }
 }
