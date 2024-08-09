c712fe963c0a2bf1531a6f051cade2b731a2a790
CeloAraujo <celoparaujo99@gmail.com>
2024-08-09 19:22:22 -0300

    Validação de formulário para chamado-create

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-chamado-create',
+  selector: "app-chamado-create",
   templateUrl: "./chamado-create.component.html",
-  styleUrls: ["./chamado-create.component.css"]
+  styleUrls: ["./chamado-create.component.css"],
 })
 export class ChamadoCreateComponent implements OnInit {
+  chamadoForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.chamadoForm = this.fb.group({
+      prioridade: this.fb.control(null, [Validators.required]),
+      status: this.fb.control(null, [Validators.required]),
+      titulo: this.fb.control(null, [Validators.required]),
+      observacoes: this.fb.control(null, [Validators.required]),
+      tecnico: this.fb.control(null, [Validators.required]),
+      cliente: this.fb.control(null, [Validators.required]),
+    });
   }
 
+  validaCampos(): boolean {
+    return this.chamadoForm.valid;
+  }
 }
