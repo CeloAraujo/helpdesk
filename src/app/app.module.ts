3e2ed35f19dff38122748d80b8c3fd6691eee9d2
marcelo.araujo <marcelo.araujo@memora.com.br>
2024-08-12 15:43:05 -0300

    componente chamado-update criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,12 +47,13 @@ import { ClienteUpdateComponent } from "./components/cliente/cliente-update/clie
 import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
 
 
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent, LoginComponent, TecnicoCreateComponent, TecnicoUpdateComponent, TecnicoDeleteComponent,ClienteCreateComponent,ClienteDeleteComponent,ClienteUpdateComponent,ClienteListComponent, ChamadoListComponent, ChamadoCreateComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent, LoginComponent, TecnicoCreateComponent, TecnicoUpdateComponent, TecnicoDeleteComponent,ClienteCreateComponent,ClienteDeleteComponent,ClienteUpdateComponent,ClienteListComponent, ChamadoListComponent, ChamadoCreateComponent, ChamadoUpdateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { Chamado } from "src/app/models/chamado";
+import { Cliente } from "src/app/models/cliente";
+import { Tecnico } from "src/app/models/tecnico";
+import { ChamadoService } from "src/app/services/chamado.service";
+import { ClienteService } from "src/app/services/cliente.service";
+import { TecnicoService } from "src/app/services/tecnico.service";
+
+@Component({
+  selector: "app-chamado-create",
+  templateUrl: "./chamado-update.component.html",
+  styleUrls: ["./chamado-update.component.css"],
+})
+export class ChamadoUpdateComponent implements OnInit {
+
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  chamadoForm: FormGroup;
+  clientes:Cliente[]
+  tecnicos:Tecnico[]
+
+  constructor(
+    private fb: FormBuilder,
+    private service: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.chamadoForm = this.fb.group({
+      prioridade: this.fb.control(null, [Validators.required]),
+      status: this.fb.control(null, [Validators.required]),
+      titulo: this.fb.control(null, [Validators.required]),
+      observacoes: this.fb.control(null, [Validators.required]),
+      tecnico: this.fb.control(null, [Validators.required]),
+      cliente: this.fb.control(null, [Validators.required]),
+    });
+
+    this.findAllClientes()
+    this.findAllTecnicos()
+  }
+
+  update(): void {
+    if (this.chamadoForm.valid) {
+      this.service.update(this.chamado.id,this.chamadoForm.value).subscribe(
+        () => {
+          this.toast.success("Chamado cadastrado com sucesso", "Cadastro");
+          this.router.navigate(["chamados"]);
+        },
+        (ex) => {
+          this.toast.error(ex.error.message);
+        }
+      );
+    }
+  }
+  findAllClientes():void{
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
+  findAllTecnicos():void{
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+  validaCampos(): boolean {
+    return this.chamadoForm.valid;
+  }
+}
